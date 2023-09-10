2f886d08782cfb64603c54d438348a62e1e53cc6
AlexandrAguilarC <alexandra10aguilarc@gmail.com>
2023-09-10 07:18:10 -0300

    Agregamos controlador, método y ruta para crear reacciones.

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   resources :adventures
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  post '/new_user_reaction', to: 'reactions#new_user_reaction', as: 'new_user_reaction'
   # Defines the root path route ("/")
   root "adventures#index"
 end
