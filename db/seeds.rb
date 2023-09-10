d38925452e8fa7aa761621b1d1dab9e865f83744
AlexandrAguilarC <alexandra10aguilarc@gmail.com>
2023-09-10 06:48:56 -0300

    Agregamos datos de prueba para adventure.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+    puts 'Creating Country...'
+    until Country.count == 20 do
+        Country.create(name: Faker::Address.country) if !Country.pluck(:name).include?(Faker::Address.country)
+    end
+
+    countries = Country.all
+
+
+    puts 'Creating Adventure...'
+    until Adventure.count == 100 do
+        Adventure.create(
+            title: Faker::Book.title, 
+            description: Faker::Lorem.paragraph_by_chars(number: 200, supplemental: false), 
+            when_went: Faker::Date.between(from: 10.years.ago, to: Date.today), 
+            country_id: countries.sample.id)
+    end
